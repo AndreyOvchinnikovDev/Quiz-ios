0dcc8d164db7bbc52737b1871541e7ab4adaf6a5
AndreyOvchinnikovDev <suetnoy@bk.ru>
2023-01-29 05:48:10 +0300

    commit sprint_06 final

--- MovieQuiz/Services/QuestionFactoryDelegate.swift
+++ MovieQuiz/Services/QuestionFactoryDelegate.swift
@@ -5,8 +5,8 @@
 //  Created by Andrey Ovchinnikov on 03.01.2023.
 //
 
-import Foundation
-
 protocol QuestionFactoryDelegate : AnyObject {
     func didReceiveNextQuestion(question: QuizQuestion?)
+    func didLoadDataFromServer()
+    func didFailToLoadData(with error: Error)
 }
